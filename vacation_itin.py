c4ac84e477c94813b3444e4449e43f2f933d31f6
Melissa Lee <mmlee246@gmail.com>
2016-11-18 10:18:05 -0600

    finished networkx, added plotting for streets and address (single)

--- vacation_itin.py
+++ vacation_itin.py
@@ -11,35 +11,35 @@ import matplotlib.pyplot
 class vacation_itinerary:
 
 	def __init__(self,city_file,attractions_file,**kwargs):
-
 		# reads road network file
 		self.ds = pandas.read_csv(city_file)
 		# reads attraction address file
 		self.attr = pandas.read_csv(attractions_file)
-
+		# creates GeoPlotter object
+		self.DSmap = GeoPlotter()
 		# creates networkx graph
 		self.createNetworkxGraph()
 
+
 	# creates the networkx graph for the road network
 	def createNetworkxGraph(self): 
-
         #creates a networkx graph
 	    self.gd = networkx.DiGraph()
 
 	    # F Streets
 	    self.Fds = self.ds[self.ds.oneway=='F']
-	    self.FstartPTS = zip(str(self.Fds.startlon.values).strip('[]').split(), str(self.Fds.startlat.values).strip('[]').split())
-	    self.FendPTS = zip(str(self.Fds.endlon.values).strip('[]').split(), str(self.Fds.endlat.values).strip('[]').split())
+	    self.FstartPTS = zip(self.Fds.startlon.values, self.Fds.startlat.values)
+	    self.FendPTS = zip(self.Fds.endlon.values, self.Fds.endlat.values)
 
 	    # T Streets
 	    self.Tds = self.ds[self.ds.oneway=='T']
-	    self.TstartPTS = zip(str(self.Tds.startlon.values).strip('[]').split(), str(self.Tds.startlat.values).strip('[]').split())
-	    self.TendPTS = zip(str(self.Tds.endlon.values).strip('[]').split(), str(self.Tds.endlat.values).strip('[]').split())
+	    self.TstartPTS = zip(self.Tds.startlon.values, self.Tds.startlat.values)
+	    self.TendPTS = zip(self.Tds.endlon.values, self.Tds.endlat.values)
 
 	    # B Streets
 	    self.Bds = self.ds[self.ds.oneway=='B']
-	    self.BstartPTS = zip(str(self.Bds.startlon.values).strip('[]').split(), str(self.Bds.startlat.values).strip('[]').split())
-	    self.BendPTS = zip(str(self.Bds.endlon.values).strip('[]').split(), str(self.Bds.endlat.values).strip('[]').split())
+	    self.BstartPTS = zip(self.Bds.startlon.values, self.Bds.startlat.values)
+	    self.BendPTS = zip(self.Bds.endlon.values, self.Bds.endlat.values)
 
 	    # Simplification - all roads speed = 50mph
 	    timeF = scipy.ones((len(self.Fds.miles.values),1))*50.
@@ -63,9 +63,79 @@ class vacation_itinerary:
 	    self.gd.add_edges_from(B_edges1) 
 	    self.gd.add_edges_from(B_edges2)
 
+
 	# finds the shortest path between two nodes using Dijkstra's algorithm in networkx
 	def getSPNetworkx(self,startnode,destnode):
 		return networkx.shortest_path(self.gd,source=startnode,target=destnode,weight='weight')
+
+
+	# plots street network using geoplotter
+	def drawStreetNetwork(self):
+		# sets up edge lon and lat for passing into drawLines function from geoplotter
+		street_lines = [[list(b) for b in c] for c in self.gd.edges()]
+		# draws the street network
+		self.DSmap.drawLines(lines = street_lines,color = 'b',linewidth = 0.3)
+
+	def zoomToFit(self):
+		# gets range of longitude and latitude w.r.t. the attractions and zooms map
+		lonLatRan = self.getLonLatRange()
+		self.DSmap.setZoom(lonLatRan[0][0], lonLatRan[1][0], lonLatRan[0][1], lonLatRan[1][1])
+		ax = self.DSmap.getAxes()
+		ax.axes.get_xaxis().set_ticks([])
+		ax.get_yaxis().set_ticks([])
+		matplotlib.pyplot.show()
+
+
+	# plots a single address
+	def drawAddress(self,addressLon,addressLat,marksz=75):
+		cND = self.findClosestNode(addressLat,addressLon)
+		self.DSmap.drawPoints(lat=cND[1],lon=cND[0],color='r',s=marksz)
+
+
+	# finds longitude and latitude range w.r.t. lon and lat of attractions
+	def getLonLatRange(self):
+		return [[min(self.attr.lon.values),max(self.attr.lon.values)],[min(self.attr.lat.values),max(self.attr.lat.values)]]
+
+
+	# finds the closest node to the set of addresses
+	def findClosestNode(self,placeLat,placeLon):
+	    pos_nodes = scipy.array([])
+	    lonLatRange = self.getLonLatRange()
+	    lonRange = lonLatRange[0]
+	    latRange = lonLatRange[1]
+	    # a little more than 2 seconds longitude and latitude
+	    latdf = 0.0005
+	    londf = 0.0005
+	    ct = 0
+	    listLon = list(zip(*self.gd.nodes())[0])
+	    listLat = list(zip(*self.gd.nodes())[1])
+	    
+	    while len(pos_nodes)==0 and ((latdf<scipy.absolute(latRange[0]-latRange[1])/2) and (londf<scipy.absolute(lonRange[0]-lonRange[1]))):
+	        pos_nodes = scipy.array([])
+	        ct = ct + 1
+	        latidx1 = scipy.where((listLat<placeLat+ct*latdf))[0]
+	        tempLatList = [listLat[a] for a in latidx1]
+	        tempLonList = [listLon[a] for a in latidx1]
+	        latidx = scipy.where(tempLatList>placeLat-ct*latdf)[0]
+	        tempLatList = [tempLatList[b] for b in latidx]
+	        tempLonList = [tempLonList[b] for b in latidx]
+	        lonidx1 = scipy.where(tempLonList>placeLon-ct*londf)[0]
+	        tempLatList = [tempLatList[c] for c in lonidx1]
+	        tempLonList = [tempLonList[c] for c in lonidx1]
+	        lonidx = scipy.where(tempLonList<placeLon+ct*londf)[0]
+	        pos_nodes = scipy.array([[tempLonList[d],tempLatList[d]] for d in lonidx])
+	        
+	    if len(pos_nodes)==0:
+	        return None
+	    else:
+	        dist = scipy.sqrt(scipy.sum(scipy.array([pos_nodes.T[0]-placeLon, pos_nodes.T[1]-placeLat])**2,axis=0))
+	        # returns the name of the closest node as a string
+	        #return str(pos_nodes[0][scipy.where(dist==min(dist))][0])+' '+str(pos_nodes[1][scipy.where(dist==min(dist))][0])
+	        # recall, [lon, lat]
+	        closeNode = scipy.array([pos_nodes.T[0][scipy.where(dist==min(dist))][0],
+	                            pos_nodes.T[1][scipy.where(dist==min(dist))][0]])
+	        return closeNode
+	
 
 	# finds the total reward given an itinerary
 	def getItineraryReward(self,itin):
@@ -80,6 +150,7 @@ class vacation_itinerary:
 
 		return 0
 
+
 	def savings_alg(self):
 
 		# Compute shortest path between source and all other reachable nodes for a weighted graph.
@@ -92,7 +163,11 @@ class vacation_itinerary:
 
 if __name__ == '__main__':
 	austin_itinerary = vacation_itinerary(city_file='austin_edges.csv',attractions_file='austin_nodes.csv')
-	temp = austin_itinerary.attr.attraction.values
-	print temp
-	austin_itinerary.getItineraryReward(itin=temp)
+	austin_itinerary.drawStreetNetwork()
+	austin_itinerary.drawAddress(addressLon=austin_itinerary.attr.lon.values[0],addressLat=austin_itinerary.attr.lat.values[0])
+	austin_itinerary.zoomToFit()
+	
+	#austin_itinerary.getItineraryReward(itin=temp)
+
+	
 
